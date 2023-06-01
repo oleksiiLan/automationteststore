c217678d8d5804637c20864b3dec3ba982dcbea9
“o_lan” <lant91a@gmail.coml>
2023-06-01 03:28:58 +0200

    updated homeworkR&A: separated for two test

--- cypress/e2e/homeworkR&A.cy.js
+++ cypress/e2e/homeworkR&A.cy.js
@@ -1,4 +1,4 @@
-describe("Registration and Autorization test", () => {
+describe.skip("Registration and Autorization test", () => {
   it("Test", () => {
     cy.visit("https://automationteststore.com/");
 
@@ -82,3 +82,77 @@ describe("Registration and Autorization test", () => {
     });
   });
 });
+
+
+
+describe("Registration and Autorization test", () => {
+  for (let i = 88; i <= 90; i++) {
+
+    it(`Registration: Olexiy${i}`, () => {
+      cy.visit("https://automationteststore.com/");
+      cy.get("#customer_menu_top > li").click();
+      cy.get("#accountFrm button").click();
+      cy.url().should("eq","https://automationteststore.com/index.php?rt=account/create");
+      //create account form
+      cy.log("Your Personal Details section");
+      cy.get("#AccountFrm_firstname").type(`Olexiy${i}`);
+      cy.get("#AccountFrm_lastname").type(`Lan${i}`);
+      cy.get("#AccountFrm_email").type(`testmail${i}@test.com`);
+      cy.get("#AccountFrm_telephone").type("+380987654321");
+  
+      cy.log("Your Address section");
+      cy.get("select#AccountFrm_country_id").select("Ukraine");
+      cy.get("select#AccountFrm_zone_id").select(`L\'viv`);
+      cy.get("#AccountFrm_city").type("Lviv");
+      cy.get("#AccountFrm_address_1").type("Rynok Square, 1");
+      cy.get("#AccountFrm_postcode").type("79000");
+  
+      cy.log("Login Details section");
+      cy.get("#AccountFrm_loginname").type(`Olexiy${i}`);
+      cy.get("#AccountFrm_password").type(`Olexiy${i}`);
+      cy.get("#AccountFrm_confirm").type(`Olexiy${i}`);
+  
+      cy.get("#AccountFrm_newsletter1").click();
+      cy.get("#AccountFrm_agree").click();
+  
+      //created account 
+      cy.log("Success");
+      cy.get(".col-md-2.mt20.mb40 button").click();
+      cy.url().should("eq","https://automationteststore.com/index.php?rt=account/success");
+      cy.get("#maincontainer .maintext").should(
+      "have.text",
+      " Your Account Has Been Created!"
+      );
+  
+      //logout from created account
+      cy.get("#customer_menu_top").click();
+      cy.get(".nav-dash li").last().click();
+      cy.get("#maincontainer .maintext").should(
+      "have.text",
+      " Account Logout"
+      );
+      //cy.get("#customernav li").click();
+    })
+
+
+    it(`Autorization: Olexiy${i}`, () => {
+      //autorization to the created account
+      cy.log("Autorization");
+      cy.visit('https://automationteststore.com/index.php?rt=account/login')
+      cy.url().should(
+      "eq",
+      "https://automationteststore.com/index.php?rt=account/login"
+      );
+      cy.get("#loginFrm_loginname").clear().type(`Olexiy${i}`);
+      cy.get("#loginFrm_password").clear().type(`Olexiy${i}`);
+      cy.get('#loginFrm button[type="submit"]').click();
+  
+      cy.get(".heading1 > span").should(($lis) => {
+        expect($lis).to.have.length(2);
+        expect($lis.eq(0)).to.contain(" My Account");
+        expect($lis.eq(1)).to.contain(`Olexiy${i}`);
+      });
+      //cy.get(".nav-dash li").last().click();
+    })
+  }
+})
